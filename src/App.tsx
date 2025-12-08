93b6987c4ced9e9d51fe7b99419bfee75efb2b52
rashad <rashadhusein440@gmail.com>
2025-12-08 19:38:08 +0200

    - Add SignIn, SignUp,

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,88 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+import OTPVerification from "./pages/auth/OTPVerification";
+import { ROUTES } from "./constants/routes";
+
+// Create a custom theme that matches Tailwind colors
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#93b4de",
+      main: "#145db8",
+      dark: "#0e4283",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#586372",
+      main: "#05162c",
+      dark: "#04101f",
+      contrastText: "#fff",
+    },
+    error: {
+      light: "#fd8688",
+      main: "#fc4b4e",
+      dark: "#b33537",
+      contrastText: "#fff",
+    },
+    warning: {
+      light: "#ffc46e",
+      main: "#ffa726",
+      dark: "#b5771b",
+      contrastText: "#fff",
+    },
+    info: {
+      light: "#62b5ec",
+      main: "#1490e3",
+      dark: "#0e66a1",
+      contrastText: "#fff",
+    },
+    success: {
+      light: "#87c98a",
+      main: "#4caf50",
+      dark: "#367c39",
+      contrastText: "#fff",
+    },
+    grey: {
+      50: "#f5f6f7",
+      100: "#e6e8ea",
+      200: "#d0d4d8",
+      300: "#bbc1c7",
+      400: "#adb5bc",
+      500: "#99a2ab",
+      600: "#8b939c",
+      700: "#6d7379",
+      800: "#54595e",
+      900: "#404448",
+    },
+  },
+  typography: {
+    fontFamily: '"Noto Sans Georgian", sans-serif',
+  },
+});
 
 function App() {
-
   return (
-    <>
-      <div>
-        <h1>How is it going</h1>
-      </div>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          {/* Auth Routes */}
+          <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+          <Route path={ROUTES.VERIFY_OTP} element={<OTPVerification />} />
+
+          {/* Default Route */}
+          <Route
+            path={ROUTES.HOME}
+            element={<Navigate to={ROUTES.SIGN_IN} replace />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;

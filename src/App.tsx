148f8691d6b4d94a28b7ff869128e49c69fe243b
Reda Salem <redasalem287@gmail.com>
2025-12-09 04:00:51 +0200

    Resolve merge conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import './App.css'
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import BookingPage from './pages/BookingPage';
+import Home from './pages/Home';
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>How is it going</h1>
-      </div>
-    </>
+ <BrowserRouter>
+
+  {/* import navbar here */}
+
+ <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/BookingPage" element={<BookingPage />} />
+ </Routes>
+
+  {/* import footer here */}
+
+
+ </BrowserRouter>
   )
 }
 
